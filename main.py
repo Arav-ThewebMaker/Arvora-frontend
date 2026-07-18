3cfe6f0f2551d9725bfa359dfb0fa644324a15ee
Arav-ThewebMaker <pramilaparikh9@gmail.com>
2026-07-18 19:18:23 +0530

    Fix CORS for GitHub Pages

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://arav-thewebmaker.github.io",
-        "http://127.0.0.1:5500"
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
     ],
     allow_credentials=True,
     allow_methods=["*"],
