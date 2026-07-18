14ceedea7998aab04edf42894dddfd0cbc9ec079
Arav-ThewebMaker <pramilaparikh9@gmail.com>
2026-07-18 19:05:36 +0530

    Allow GitHub Pages CORS

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
+        "https://arav-thewebmaker.github.io",
+        "http://127.0.0.1:5500"
     ],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 
