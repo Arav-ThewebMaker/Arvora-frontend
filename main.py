dfa939704a2f5bc68ba29b7f406813eb9245eef3
Arav-ThewebMaker <pramilaparikh9@gmail.com>
2026-07-18 19:26:39 +0530

    Test CORS

--- main.py
+++ main.py
@@ -17,14 +17,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://arav-thewebmaker.github.io",
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_credentials=False,
 )
 
 
